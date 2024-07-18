8f7ef6a003d183343f854d59dac9938e2ced0c10
SWATHA27 <155546524+SWATHA27@users.noreply.github.com>
2024-07-18 14:38:43 +0530

    backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-eks"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+   
+  }
+}
